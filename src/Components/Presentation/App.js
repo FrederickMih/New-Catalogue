11f0acc7f13f1e1f60faf26c1b62d933c4c4dcf2
FrederickMih <fredtimermih@gmail.com>
2021-09-10 19:59:09 +0100

    Add main Page footers and styles

---
+++ src/Components/Presentation/App.js
@@ -0,0 +1,7 @@
+const App = () => (
+  <div className="app-div">
+    <h1>app component</h1>
+  </div>
+);
+
+export default App;
---
+++ src/Components/Presentation/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/Footer.css';
+
+const Footer = () => (
+  <footer className="footer">
+    Copyright
+    {' '}
+    &copy;
+    {' '}
+    <a href="https://github.com/iliebabcenco" alt="github link">Mih Frederick</a>
+  </footer>
+);
+
+export default Footer;
