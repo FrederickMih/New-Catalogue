cb511110eac397346001068cf5be65cc60607792
FrederickMih <fredtimermih@gmail.com>
2021-09-10 18:27:37 +0100

    Define all redux actions

---
+++ src/Actions/index.js
@@ -0,0 +1,51 @@
+import axios from 'axios';
+
+export const SEARCH_ARTICLE = 'SEARCH_ARTICLE';
+export const FETCH_ARTICLES = 'FETCH_ARTICLES';
+export const FETCH_MORE_ARTICLES = 'FETCH_MORE_ARTICLES';
+export const LOADING = 'LOADING';
+export const FILTER_ARTICLE = 'FILTER_ARTICLE';
+
+const apiKey = 'Q2yqQDBUwiAJgU0GhygY5N5q4rLITbMzOSFIt3yOT8chT-pr';
+
+const searchArticle = (keyword) => (dispatch) => {
+  dispatch({
+    type: SEARCH_ARTICLE,
+    payload: keyword,
+  });
+};
+
+const fetchArticles = (keyword, page = 1) => (dispatch) => {
+  const req1 = new Request(`https://api.currentsapi.services/v1/search?keywords=${keyword}&language=en&apiKey=${apiKey}&page=${page}`);
+  axios.get(req1)
+    .then((response) => dispatch({
+      type: FETCH_ARTICLES,
+      payload: response.data,
+    }))
+    .catch((err) => (
+      console.log(err)
+    ));
+};
+
+const fetchMoreArticles = (id) => (dispatch) => {
+  const req2 = new Request(`https://api.currentsapi.services/v1/latest-news?language=us&apiKey=${apiKey}&i=${id}`);
+  axios.get(req2)
+    .then((response) => dispatch({
+      type: FETCH_MORE_ARTICLES,
+      payload: response.data,
+    }))
+    .catch((err) => (console.log(err)));
+};
+
+const setLoading = () => ({
+  type: LOADING,
+});
+
+const filterArticle = (filter) => ({
+  type: FILTER_ARTICLE,
+  payload: filter,
+});
+
+export {
+  searchArticle, fetchArticles, fetchMoreArticles, setLoading, filterArticle,
+};
