8310d42274848bfb6837230321a8af7874b70d72
FrederickMih <fredtimermih@gmail.com>
2021-09-10 21:54:49 +0100

    Set up the Routes

--- src/Components/Presentation/App.js
+++ src/Components/Presentation/App.js
@@ -1,9 +1,11 @@
 import NavBar from './NavBar';
 import Footer from './Footer';
+import Routes from '../../routes/Routes';
 
 const App = () => (
   <div className="app-div">
     <NavBar />
+    <Routes />
     <Footer />
   </div>
 );
---
+++ src/routes/Routes.js
@@ -0,0 +1,13 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React from 'react';
+
+const Routes = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route />
+      {/* <Route exact path="/" component={} /> */}
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Routes;
