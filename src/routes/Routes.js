737d0e51301974cb7ce9eca0e105d7daea343746
FrederickMih <fredtimermih@gmail.com>
2021-09-13 01:03:14 +0100

    Add a route to the article detailed page

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import React from 'react';
 import Main from '../Components/Container/Main';
+import MoreDetailedArticle from '../Components/Presentation/MoreDetailedArticle';
 
 const Routes = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/" component={Main} />
+      <Route exact path="/article/:id" component={MoreDetailedArticle} />
     </Switch>
   </BrowserRouter>
 );
